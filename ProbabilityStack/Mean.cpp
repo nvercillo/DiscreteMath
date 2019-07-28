ab1b973c81b45b8e0e856b82c773addf1bc9c935
svercillo <40708666+svercillo@users.noreply.github.com>
2019-07-28 12:00:34 -0400

    Create Mean.cpp
    This stack creates a stack with a random generator under certain conditions.

---
+++ ProbabilityStack/Mean.cpp
@@ -0,0 +1,65 @@
+#include "Mean.h"
+
+
+
+int main (int, char*argv[]){
+        int random, stacksize;
+          /* initialize random seed: */
+//        srand (time(NULL));
+
+        vector <int> stack;
+        stacksize = 500;
+        int count =-1;
+        while (count< stacksize){
+/* generate secret number between 1 and 10:
+* change this to 0 to 110    % 111 */
+                random = rand() % 11;
+                if (random ==9 || random == 18){
+                        if(count !=0) {
+                                int old = stack[count];
+                                stacksize = stacksize + old;
+                        }
+
+                } else if (random == 13) {
+                        if (count !=0){
+                                int old = stack[count];
+                                // minus 1?
+                // what if the subtracted number goes to zero?
+
+                                if (count - old < 0){
+                                        for (int i=0; i<= count; i++){
+                                                stack.pop_back();
+                                        }
+                                        count = 0;
+                                } else {
+                                        for (int i= count; i>= count - old; i--){
+                                                stack.pop_back();
+                                        }
+                                        count = count - old;
+                                }
+                        }
+                } else {
+                        count++;
+                        stack.push_back(random);
+                }
+        }
+
+//      for (int i :stack)
+//              cout <<i <<endl;
+
+        int sum=0;
+        for (int j : stack){
+                sum = sum + j;
+        }
+//      cout<< sum << endl;
+
+cout << "MY stack size is "  << stacksize <<endl;
+cout << "My sum  is " << sum << endl;
+        float  mean = (float )sum / (float) stacksize;
+        cout << "mean is " << mean
+
+//      cout<< "Mean is:   " << mean << endl;
+//      cout << stack.size()
+        return 0;
+i
+                       
