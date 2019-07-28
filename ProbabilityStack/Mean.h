b67a42d09e102205d9650ea98a33f55934054652
svercillo <40708666+svercillo@users.noreply.github.com>
2019-07-28 12:01:48 -0400

    Create Mean.h

---
+++ ProbabilityStack/Mean.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <cstring>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <time.h>
+#include <stdlib.h>
+
+using std:: cout;
+using std :: endl;
+using std:: string;
+using std :: vector;
+using std:: ifstream;
+using std :: cin;
+
