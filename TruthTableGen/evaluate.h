763883dad32b62468c1da5c6893189481a6b4c93
svercillo <40708666+svercillo@users.noreply.github.com>
2019-07-28 11:45:44 -0400

    Create evaluate.h

---
+++ TruthTableGen/evaluate.h
@@ -0,0 +1,20 @@
+// All functions were incorporated in main class in evaluate.cpp
+
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <cstring>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <fstream>
+
+
+using std:: cout;
+using std :: endl;
+using std:: string;
+using std :: vector;
+using std:: ifstream;
+using std :: cin;
